b5f364330ed6b4bd4103c22e5b4e75e4ce9c354d
Shefcoding <103627787+Shefcoding@users.noreply.github.com>
2022-06-07 18:41:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-Goalscorer1 = 'Gullit'
-Goalscorer2 = 'Van Basten'
-Goal_0 = 32
-Goal_1 = 54
-Scorers = Goalscorer1 + ' '+ str(Goal_0) + ','+ Goalscorer2 + ' '+ str(Goal_1)
-print(Scorers)
-report = f'{Goalscorer1} scored in the {Goal_0}nd minute \n{Goalscorer2} scored in the {Goal_1}th minute '
+Goalscorer1 = 'Ruud Gullit'
+Goalscorer2 = 'Marco van Basten'
+goal_0 = 32
+goal_1 = 54
+scorers = Goalscorer1 +' '+ str(goal_0) + ', '+ Goalscorer2 + ' '+ str(goal_1)
+print(scorers)
+report = f'{Goalscorer1} scored in the {goal_0}nd minute\n{Goalscorer2} scored in the {goal_1}th minute'
 
 print(report)
 player = 'Ronald Koeman'
-first_name = player[player.find('Ronald'):6]
+first_name_search = player.find(' ')
+first_name = player[:first_name_search]
+print(first_name)
 
-last_name_len = len(player[player.find('Koeman'):13])
+last_name_len =len(player[first_name_search:])-1
+print(last_name_len)
 
-
-name_short = player[0]+'. '+ player[player.find('Koeman'):13]
-chant = f'\t {first_name}!' * len(first_name)
-good_chant= chant[len(chant)-1] != ' '
-print(good_chant)
+name_short = player[0]+'.'+ player[first_name_search:]
+chant = (f'{first_name}! ' *len(first_name))[:-1] 
+print(chant)
+good_chant= chant[-1] != ' '
+print(good_chant) 
